592c1f9922b1e8840ce05d237750485a68163184
DRAGON99HEAD <d99h990@gmail.com>
2026-03-17 02:00:06 -0300

    Implement parry system and enhance defense logic
    Added parry mechanics and improved defense calculations.

--- basic_python_game.py
+++ basic_python_game.py
@@ -2,6 +2,19 @@ import os
 import random
 import readchar
 
+counter = False
+slime_kills = 0
+skeleton_kills = 0
+dragon_kills = 0
+goblin_kills = 0
+ini_run = True
+bow_d = 10
+broken = 0
+dp = 0
+xp_earned = 0
+parrying = False
+parry_percent = 0
+
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -15,6 +28,9 @@ class jogador:
         self.mana = 100
         self.defesa_base = 5
         self.defesa = 5
+        self.duo_def = []
+        self.defesa_parry = 0
+        self.parry_ini = []
         self.inteligencia = 10
         self.dano_base = 5
         self.l_pocao = 2
@@ -23,6 +39,8 @@ class jogador:
         self.xp = 0
         self.max_xp = 100
         self.points = 0
+        self.m_regen = random.randint(10, 1000)
+        self.l_regen = random.randint(20, 100)
 
     def status(self):
 
@@ -31,7 +49,7 @@ class jogador:
         print("\n==================== STATUS DO JOGADOR ====================")
         print(f"Nome: {self.nome}\t\tVida: {self.atual_life}/{self.max_life}")
         print(f"Mana: {self.mana}\t\t\tInteligência: {self.inteligencia}")
-        print(f"Speed: {self.speed}\t\t\tDefesa: {self.defesa}")
+        print(f"Speed: {self.speed}\t\t\tDefesa: {self.defesa + self.defesa_parry}")
         print(f"Nível Atual: {self.level}\t\t\tXP: {self.xp}/{self.max_xp}")
         print(f"Dano Base {self.dano_base}\t\t\tPontos de Habilidade: {self.points}")
         print("===========================================================\n")
@@ -48,7 +66,6 @@ class jogador:
 
         else:
             input("Pressione ENTER para continuar ...")
-
 
 def status_upd():
 
@@ -80,12 +97,6 @@ def status_upd():
             case "5":
                 player.dano_base += 3
                 player.points -= 1
-
-
-bow_d = 10
-broken = 0
-dp = 0
-xp_earned = 0
 
 def bolsa():
 
@@ -148,7 +159,6 @@ def bolsa():
                         input(f"Sua mana foi aumentada em 100 pontos\nPressione ENTER para continuar ...")
                     elif player.m_pocao <= 0:
                         input("Você não possui poção de mana\nPressione ENTER para continuar ...")
-
 
 def attacks():
 
@@ -289,8 +299,8 @@ def attacks():
     if enemy.life > 0 and go_to == 1:
         enemy_attacks()
 
-
 def defense():
+    global parrying, parry_percent
 
     clear()
 
@@ -317,8 +327,11 @@ def defense():
                     if player.mana >= 15:
                         player.mana -= 15
                         player.defesa = int(player.defesa * 2)
+                        player.duo_def.append(5)
                         print(f"A sua defesa foi dobra de {int(player.defesa/2)} para {player.defesa}")
                         input("Pressione ENTER para continuar ...")
+
+                        enemy_attacks()
                     elif player.mana < 15:
                         print("Você não possui mana suficiente para usar o defesa dupla")
                         input("Pressione ENTER para continuar ...")
@@ -336,10 +349,12 @@ def defense():
 
             match use:
                  case "1":
+                      
                       if player.mana < 20:
                          print("Você não possui mana o suficiente para usar CURA")
                          input("Pressione ENTER para continuar ...")
                       elif player.mana >= 20:
+
                         if player.atual_life == player.max_life:
                              print("Sua vida já está no máximo")
                              input("Pressione ENTER para continuar ...")
@@ -355,6 +370,41 @@ def defense():
                                 print(f"Sua vida foi curada em {cure_points + cura} {'pontos' if cure_points + cura > 1 else 'ponto'}")
                                 input("Pressione ENTER para continuar ...")
 
+        case "3":
+
+            print("========================== PARRY ==========================")
+            print(f"Dependendo da sua velocidade você reduzira até 100% do")
+            print("ataque do inimigo e aumenta a sua defesa em 5 pontos")
+            print("por 5 rounds")
+            print("CUSTO: 30 Pontos de mana por uso")
+            print("==========================================================")
+            print("1 - Sim\n2 - Não\nDeseja usar?\n: ", end="", flush=True)
+
+            use = readchar.readkey()
+
+            clear()
+            match use:
+
+                case "1":
+
+                    if player.mana >= 30:
+
+                        parry_percent = player.speed * 0.1
+                        if parry_percent >= 100:
+
+                            parry_percent = 100
+                        parrying = True
+                        player.defesa_parry += 5
+                        player.parry_ini.append(5)
+                        player.mana -= 30
+
+                        print(f"Você irá bloquear o ataque inimigo em {int(parry_percent)}%\nSua defesa foi aumentada em 5 pontos")
+                        input("Pressione ENTER para continuar ...")
+                        
+                        enemy_attacks()
+                    else:
+                        print("Você não tem mana suficiente para usar o parry")
+                        input("Pressione ENTER para continuar ...")
 
 class inimigo():
 
@@ -412,10 +462,10 @@ class inimigo():
         elif self.intel == 0:
             xp_earned = int((self.defense / 4) + (self.force / 4) + (self.life / 4))
 
-
 def enemy_attacks():
 
-    global bow_d, broken, dp
+    global bow_d, broken, dp, parrying, parry_percent, counter
+    block = 0
 
     clear()
 
@@ -427,55 +477,81 @@ def enemy_attacks():
     if enemy.name == "DRAGÃO":
         match ea:
             case 1:
-                damage = int((enemy.force + enemy.intel) - player.defesa)
-                player.atual_life -= damage
-                print(f"O dragão usou LIGHTNING STRIKE causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.force + enemy.intel) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O dragão usou LIGHTNING STRIKE causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 2:
-                damage = int((enemy.life / 10) + (enemy.defense / 25) - player.defesa)
-                player.atual_life -= damage
-                print(f"O dragão usou INVESTIDA causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.life / 10) + (enemy.defense / 25) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O dragão usou INVESTIDA causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 3:
-                damage = int((enemy.force / 4.5) - player.defesa)
-                player.atual_life -= damage
-                print(f"O dragão usou ARRANHÃO causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.force / 4.5) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O dragão usou ARRANHÃO causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
 
     elif enemy.name == "SLIME":
         match ea:
             case 1:
-                damage = int(enemy.force * (enemy.life / 2.5) - player.defesa)
-                player.atual_life -= damage
-                print(f"O slime usou TIRO ÁCIDO causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int(enemy.force * (enemy.life / 2.5) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O slime usou TIRO ÁCIDO causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 2:
-                damage = int((enemy.life * enemy.defense + enemy.force) - player.defesa)
-                player.atual_life -= damage
-                print(f"O slime usou INVESTIDA causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.life * enemy.defense + enemy.force) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O slime usou INVESTIDA causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 3:
                 pre = enemy.life
                 enemy.life += enemy.life * 0.25
+                counter = True
                 print(f"O slime usou regrowth aumentando a sua vida em {pre * 0.25}")
                 input("Pressione ENTER para continuar ...")
 
     elif enemy.name == "GOBLIN":
         match ea:
             case 1:
-                player.atual_life -= enemy.intel
-                enemy.life += enemy.intel
+                damage = max(0, enemy.intel)
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                enemy.life += damage - block
+                counter = True
                 print(f"O goblin usou THIEF para recuperar a propria vida em {enemy.intel} e causando ao jogador {player.nome} um dano de {enemy.intel}")
                 input("Pressione ENTER para continuar ...")
             case 2:
-                damage = enemy.force * 2.5
-                player.atual_life -= damage
-                print(f"O goblin usou APUNHALADA causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.force * 2.5) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O goblin usou APUNHALADA causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 3:
-                damage = enemy.force + enemy.intel
-                player.atual_life -= damage
-                print(f"O goblin usou CHUTE causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.force + enemy.intel) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O goblin usou CHUTE causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
 
     elif enemy.name == "ESQUELETO":
@@ -486,41 +562,42 @@ def enemy_attacks():
 
         match ea:
             case 1:
-                damage = enemy.force
-                player.atual_life -= damage
+                damage = max(0, int(enemy.force - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
                 bow_d -= 1
-                print(f"O esqueleto usou FLECHADA causando {damage} de dano ao jogador {player.nome}")
+                counter = True
+                print(f"O esqueleto usou FLECHADA causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
             case 2:
-                damage = enemy.force + enemy.intel
-                player.atual_life -= damage
+                damage = max(0, int((enemy.force + enemy.intel) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
                 if broken == 1:
-                    print(f"O esqueleto usou tanto o arco que ele quebrou então está usando SOCO e causou {damage} de ao jogador {player.nome}")
+                    counter = True
+                    print(f"O esqueleto usou tanto o arco que ele quebrou então está usando SOCO e causou {damage - block} de ao jogador {player.nome}")
                     input("Pressione ENTER para continuar ...")
                     broken = 0
                 else:
-                    print(f"O esqueleto usou SOCO causando {damage} de dano ao jogador {player.nome}")
+                    counter = True
+                    print(f"O esqueleto usou SOCO causando {damage - block} de dano ao jogador {player.nome}")
                     input("Pressione ENTER para continuar ...")
             case 3:
-                damage = enemy.defense + enemy.force
-                player.atual_life -= damage
-                print(f"O esqueleto usou SCRATCH causando {damage} de dano ao jogador {player.nome}")
+                damage = max(0, int((enemy.defense + enemy.force) - (player.defesa + player.defesa_parry)))
+                if parrying == True:
+                    block = int(damage * (parry_percent / 100))
+                player.atual_life -= damage - block
+                counter = True
+                print(f"O esqueleto usou SCRATCH causando {damage - block} de dano ao jogador {player.nome}")
                 input("Pressione ENTER para continuar ...")
-
-
-round = 0
-slime_kills = 0
-skeleton_kills = 0
-dragon_kills = 0
-goblin_kills = 0
-ini_run = True
     
 while ini_run == True:
 
     clear()
     print("===========================================\n\tJOGO BÁSICO DE RPG EM PYTHON\n===========================================\n")
 
-    # Esse aqui é o único input normal do jogo, pois precisamos digitar uma palavra inteira (nome)
     nome_personagem = input(f"Insira o nome do seu personagem: ")
     player = jogador(nome_personagem)
 
@@ -531,12 +608,13 @@ while ini_run == True:
         enemy = inimigo()
 
         while player.atual_life > 0 and enemy.life > 0 and playing == True:
+
             if player.defesa < player.defesa_base:
                 player.defesa = player.defesa_base
 
             clear()
-            counter = player.atual_life
-            counter1 = enemy.life
+            if parrying == True:
+                parrying = False
         
             print(f"==================================================================\n\n\tJOGADOR: {player.nome}\tVIDA: {player.atual_life}/{player.max_life}\tMANA: {player.mana}")
             print(f"\n\tROUND: {round}\n\n\tINIMIGO: {enemy.name}\t\tVIDA: {enemy.life}\n\n======================== ESCOLHA UMA AÇÃO ========================")
@@ -556,8 +634,36 @@ while ini_run == True:
                 case "0":
                     playing = False
 
-            if counter != player.atual_life or counter1 != enemy.life:
+            if counter == True:
                 round += 1
+                player.mana += player.m_regen
+                            
+                if player.atual_life < player.max_life:
+                            
+                    cura = (player.max_life - player.atual_life) - player.l_regen
+                    if cura >= 0:
+                            
+                        player.atual_life += player.l_regen
+
+                    elif cura < 0:
+                        
+                        player.atual_life = player.atual_life + (player.l_regen + cura)
+
+                for i in range(len(player.duo_def) - 1, -1, -1):
+                    player.duo_def[i] -= 1
+
+                    if player.duo_def[i] <= 0:
+                        player.defesa = int(player.defesa / 2)
+                        player.duo_def.pop(i)
+
+                for i in range(len(player.parry_ini) - 1, -1, -1):
+                    player.parry_ini[i] -= 1
+
+                    if player.parry_ini[i] <= 0:
+                        player.defesa_parry -= 5
+                        player.parry_ini.pop(i)
+                        
+                counter = False
 
         if enemy.life <= 0:
             player.xp += xp_earned
@@ -628,7 +734,6 @@ while ini_run == True:
                     player.atual_life = player.max_life
                     print(f"\nVocê descansou e recuperou sua vida por completo e sua mana foi recuperada em {mana_regen} pontos")
                     input("Pressione ENTER pata continuar ...")
-
     
     if player.atual_life <= 0:
         clear()
